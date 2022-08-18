58e78c2cee9e2fd4e1b3ddfc628c6972948788f4
robbyxp1 <robxp@hotmail.com>
2022-08-18 21:55:28 +0100

    Add a 4.8 demo of a CSharpDLL

---
+++ ExampleAddInDLL/CSharpDLL2/CSharpDLL2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDLL2
+{
+    public class CSharpDLL2EDDClass
+    {
+        public CSharpDLL2EDDClass()
+        {
+            System.Diagnostics.Debug.WriteLine("CSharpDLL2 Made DLL instance");
+        }
+
+        public string EDDInitialise(string vstr, string dllfolder, EDDDLLInterfaces.EDDDLLIF.EDDCallBacks cb)
+        {
+            System.Diagnostics.Debug.WriteLine("CSharpDLL Init func " + vstr + " " + dllfolder);
+            System.IO.File.AppendAllText(@"c:\code\csharpdll.txt", Environment.NewLine + "Init " + vstr + " in " + dllfolder + Environment.NewLine);
+            return "1.0.0.0";
+        }
+    }
+}
